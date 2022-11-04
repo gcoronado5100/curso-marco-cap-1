296dd4856d0336408df8f86e68bc54f5190a6edb
Gabriel Coronado <74467208+gcoronado5100@users.noreply.github.com>
2022-11-03 21:14:56 -0400

    added functions on JS

--- app.js
+++ app.js
@@ -1,5 +1,48 @@
-const caja = document
-  .querySelector(".resumen-title")
-  .addEventListener("click", () => {
-    alert("Esta seccion tiene mi experiencia de trabajo profesional");
-  });
+const resumenCards = [
+  {
+    title: "Desarrollador JR.",
+    company: "Freelance.com",
+    info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem ullam dicta nihil excepturi eius.",
+  },
+  {
+    title: "Mensajero.",
+    company: "Freelance.com",
+    info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem ullam dicta nihil excepturi eius.",
+  },
+  {
+    title: "Fotografo.",
+    company: "Freelance.com",
+    info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem ullam dicta nihil excepturi eius.",
+  },
+  {
+    title: "Desarrollador JR.",
+    company: "Google",
+    info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem ullam dicta nihil excepturi eius.",
+  },
+  {
+    title: "Desarrollador SR.",
+    company: "Amazon.com",
+    info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem ullam dicta nihil excepturi eius.",
+  },
+  {
+    title: "Desarrollador SR.",
+    company: "facebook",
+    info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem ullam dicta nihil excepturi eius.",
+  },
+];
+
+/**
+ Function ForEach -> ciclo repetitivo para recorrer arreglos
+ Append => agregar un nuevo elemento de HTML al DOM -> Documento
+ AppenChild (usada en este ejercicio) Agrega un elemento de HTML como HIJO del elemeto seleccionado  
+ */
+
+resumenCards.forEach((card) => {
+  const resumenCard = document.createElement("div");
+  resumenCard.classList.add("resumen-card");
+  const content = ` <h3 class="card-title">${card.title}</h3>
+                    <h4 class="position">${card.company}</h4>
+                    <p class="card-info">${card.info}</p>`;
+  resumenCard.innerHTML = content;
+  document.querySelector(".resumen-cargo__container").appendChild(resumenCard);
+});
